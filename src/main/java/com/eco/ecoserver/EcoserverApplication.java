a6f87c3ea47f770d2fb01bf0248c3bb958108d19
ssoheeh <prinso@naver.com>
2024-08-19 15:52:24 +0900

    feat: project settings

---
+++ src/main/java/com/eco/ecoserver/EcoserverApplication.java
@@ -0,0 +1,13 @@
+package com.eco.ecoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eco/ecoserver/EcoserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eco.ecoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
