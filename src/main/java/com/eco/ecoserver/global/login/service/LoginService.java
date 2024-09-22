11e98427f29bc67b8234efa423a41c841b81c6e5
ssoheeh <prinso@naver.com>
2024-09-22 17:48:03 +0900

    fixed error

--- src/main/java/com/eco/ecoserver/global/login/service/LoginService.java
+++ src/main/java/com/eco/ecoserver/global/login/service/LoginService.java
@@ -18,7 +18,7 @@ public class LoginService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    @Overrides
+    @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("해당 이메일이 존재하지 않습니다."));
