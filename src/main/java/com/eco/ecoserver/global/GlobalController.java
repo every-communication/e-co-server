a8064f6097322bc57e3b32c0dd9fdc316b416b9b
jbaem <101052774+jbaem@users.noreply.github.com>
2024-10-07 14:48:19 +0900

    Update GlobalController.java

--- src/main/java/com/eco/ecoserver/global/GlobalController.java
+++ src/main/java/com/eco/ecoserver/global/GlobalController.java
@@ -2,7 +2,9 @@ package com.eco.ecoserver.global;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class GlobalController {
     @GetMapping("/health")
     public ResponseEntity<String> health() {
