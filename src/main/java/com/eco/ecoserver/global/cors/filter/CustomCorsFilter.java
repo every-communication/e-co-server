216fb37198b71aa6aac0e52cc8482232d6b59f87
jbaem <101052774+jbaem@users.noreply.github.com>
2024-11-12 02:20:55 +0900

    Update CustomCorsFilter.java
    - origin 와일드카드 제거

--- src/main/java/com/eco/ecoserver/global/cors/filter/CustomCorsFilter.java
+++ src/main/java/com/eco/ecoserver/global/cors/filter/CustomCorsFilter.java
@@ -14,14 +14,23 @@ import java.util.List;
 @Slf4j
 public class CustomCorsFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        List<String> allowedOrigins = Arrays.asList(
+                "https://api.e-co.rldnd.net",
+                "https://e-co.rldnd.net",
+                "http://localhost:3000",
+                "http://localhost:8080",
+                "ws://localhost:8080",
+                "wss://api.e-co.rldnd.net"
+        );
+
         String origin = request.getHeader("Origin");
         log.info("CORS Filter triggered for origin: {}", origin);
 
-        if (origin != null) {
+        if (origin != null && allowedOrigins.contains(origin)) {
             response.setHeader("Access-Control-Allow-Origin", origin);
         }
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods","*");
+        response.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, PATCH, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, Authorization");
