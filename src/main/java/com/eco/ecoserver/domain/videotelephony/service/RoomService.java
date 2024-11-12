1f829a02ee44bea1062e43596a11a962b9aff565
jbaem <101052774+jbaem@users.noreply.github.com>
2024-11-12 21:34:47 +0900

    Update | 방 생성시 이벤트 발생

--- src/main/java/com/eco/ecoserver/domain/videotelephony/service/RoomService.java
+++ src/main/java/com/eco/ecoserver/domain/videotelephony/service/RoomService.java
@@ -54,7 +54,7 @@ public class RoomService {
         room.setFriendId(friendId);
         room.setCode(UUID.randomUUID().toString());
 
-        //notificationService.createVideoTelephonyNotification(room);
+        notificationService.createVideoTelephonyNotification(room);
         return roomRepository.save(room);
     }
 
